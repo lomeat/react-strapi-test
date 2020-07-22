6112a67754b98262791e7d5a0400cb601e11a197
lomeat <lom3at@gmail.com>
2020-07-22 05:50:15 +0300

    moves games to another page

---
+++ frontend/pages/games.js
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+import Link from "next/link";
+import Head from "next/head";
+import { useEffect, useState } from "react";
+
+import { Wrapper } from "../style";
+
+export default function GamesPage() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:1337/games");
+      const result = await response.json();
+      setGames(result);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+        ></link>
+      </Head>
+      <Wrapper>
+        <div>
+          <h1>Favourite games</h1>
+          <Link href="/">Home</Link>
+        </div>
+        <List>
+          {games.map((game) => (
+            <Item key={game.id} game={game} />
+          ))}
+        </List>
+      </Wrapper>
+    </>
+  );
+}
+
+const List = styled.div`
+  display: grid;
+  width: 1000px;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  gap: 40px;
+`;
+
+const Item = ({ game }) => {
+  console.log(game);
+
+  return (
+    <>
+      <h1>{game.title}</h1>
+      <p>{game.description}</p>
+      <h3>{game.releaseDate}</h3>
+      <img src={game.poster.name}></img>
+    </>
+  );
+};
---
+++ frontend/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+`;
