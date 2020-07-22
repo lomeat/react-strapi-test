010f9feb48dcee57615a99b66de53173a6420b58
lomeat <lom3at@gmail.com>
2020-07-22 05:50:37 +0300

    fetch games from strapi api

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,8 @@ import styled, { createGlobalStyle } from "styled-components";
 import Head from "next/head";
 import Link from "next/link";
 
+import { Wrapper } from "../style";
+
 const GlobalStyle = createGlobalStyle`
   * {
     outline: none;
@@ -11,10 +13,6 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 export default function IndexPage() {
-  const click = () => {
-    console.log("hello another world");
-  };
-
   return (
     <>
       <GlobalStyle />
@@ -27,36 +25,14 @@ export default function IndexPage() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Wrapper>
-        <H1 style={{ width: "200px" }}>Favourite games</H1>
-        <List>
-          {[1, 2, 3, 4, 5].map((a) => (
-            <Item key={a}>{a}</Item>
-          ))}
-        </List>
+        <div>
+          <h1>Home page</h1>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/games">Games</Link>
+        </div>
       </Wrapper>
     </>
   );
 }
-
-const H1 = styled.h1`
-  text-align: center;
-`;
-
-const Wrapper = styled.div`
-  display: flex;
-  width: 100vw;
-  height: 100vh;
-  align-items: center;
-  justify-content: space-around;
-  flex-direction: column;
-`;
-
-const List = styled.div`
-  display: grid;
-  width: 1000px;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: 1fr 1fr;
-  gap: 40px;
-`;
-
-const Item = styled.div``;
